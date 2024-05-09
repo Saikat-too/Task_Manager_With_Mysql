222bcb55b7a47f839b18c466fe8d55f776cfb82d
Saikat-too <singhasaikat37@gmail.com>
2024-05-09 19:02:31 +0600

    connecting database

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
+  user: 'root',
+  password: '',
   database: 'task_manager'
 });
 
