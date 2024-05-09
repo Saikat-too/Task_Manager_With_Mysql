3ee686e4ae7ca8d915033d57b0db3af07205adb6
Saikat-too <singhasaikat37@gmail.com>
2024-05-09 19:04:39 +0600

    fixing issue

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app =express();
-app.use (exprss.json);
+app.use (express.json);
 
 app.use('/auth' , authRoutes);
 app.use('/task' ,taskRoutes);
